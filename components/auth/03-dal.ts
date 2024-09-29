99c7050bafe5248e72f5851b76a45e6a3c398eea
Prathamesh Chougale <prathamesh17170@gmail.com>
2024-09-29 11:40:06 +0530

    update: Refactor code to import the 'cache' module from 'react' in 03-dal.ts

--- components/auth/03-dal.ts
+++ components/auth/03-dal.ts
@@ -3,7 +3,7 @@ import clientPromise from '@/mongo/clientpromise';
 import { verifySession } from './02-stateless-session'; // or './02-database-session' if using database sessions
 import { User } from '@/mongo/mongodb-schema';
 import { ObjectId } from 'mongodb';
-// import { cache } from 'react';
+import { cache } from 'react';
 
 async function getCollection(collectionName: string) {
   const client = await clientPromise;
@@ -11,19 +11,19 @@ async function getCollection(collectionName: string) {
   return db.collection(collectionName);
 }
 
-export async function getUser(): Promise<User | null> {
-  const session = await verifySession();
-  if (!session) return null;
+// export async function getUser(): Promise<User | null> {
+//   const session = await verifySession();
+//   if (!session) return null;
 
-  try {
-    const usersCollection = await getCollection('users');
-    const user = await usersCollection.findOne<User>({ _id: new ObjectId(session.userId as string) });
-    return user;
-  } catch (error) {
-    console.log('Failed to fetch user:', error);
-    return null;
-  }
-}
+//   try {
+//     const usersCollection = await getCollection('users');
+//     const user = await usersCollection.findOne<User>({ _id: new ObjectId(session.userId as string) });
+//     return user;
+//   } catch (error) {
+//     console.log('Failed to fetch user:', error);
+//     return null;
+//   }
+// }
 
 // export async function updateUser(userId: string, updateData: Partial<{ name: string, email: string }>): Promise<boolean> {
 //   try {
@@ -42,30 +42,18 @@ export async function getUser(): Promise<User | null> {
 
 // Add more data access functions as needed
 
-// export const getUser = cache(async () => {
-//   const session = await verifySession();
-//   if (!session) return null;
+export const getUser = cache(async () => {
+  const session = await verifySession();
+  if (!session) return null;
+  try{
 
-//   try {
-//     // const data = await db.query.users.findMany({
-//     //   where: eq(users.id, session.userId),
-
-//     //   // Explicitly return the columns you need rather than the whole user object
-//     //   columns: {
-//     //     id: true,
-//     //     name: true,
-//     //     email: true,
-//     //   },
-//     // });
-
-//     const usersCollection = await getCollection('users');
-//     const data = await usersCollection.find<User>({ _id: new ObjectId(session.userId) }).toArray();
-//     console.log('data:', data);
-//     const user = data[0];
-
-//     return user;
-//   } catch (error) {
-//     console.error('Failed to fetch user:', error);
-//     return null;
-//   }
-// });
+    const usersCollection = await getCollection('users');
+    const user = await usersCollection.findOne<User>({ _id: new ObjectId(session.userId as string) });    
+    return user;
+  }catch(error){
+    console.log('Failed to fetch user:', error);
+    return null;
+  }
+  
+  } 
+);
